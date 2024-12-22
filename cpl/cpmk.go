e91f62497e89c6b8916190208ac877ed44abc3ef
hippocampa <tsdhrm@outlook.com>
2024-12-22 13:43:15 +0800

    rewriting the write function (WIP)

--- cpl/cpmk.go
+++ cpl/cpmk.go
@@ -14,11 +14,46 @@ const (
 var Columns = []string{Partisipasi, Kuis, UTS, UAS, Project}
 
 type CPMK struct {
-	Values map[string]float32
+	Name     string
+	Values   map[string]float32
+	BeginCol int
+	EndCol   int
+	Row      int
 }
 
-func New(partisipasi float32, kuis float32, uts float32, uas float32, project float32) *CPMK {
+// Setter for BeginCol
+func (c *CPMK) SetBeginCol(beginCol int) {
+	c.BeginCol = beginCol
+}
+
+// Getter for BeginCol
+func (c *CPMK) GetBeginCol() int {
+	return c.BeginCol
+}
+
+// Setter for EndCol
+func (c *CPMK) SetEndCol(endCol int) {
+	c.EndCol = endCol
+}
+
+// Getter for EndCol
+func (c *CPMK) GetEndCol() int {
+	return c.EndCol
+}
+
+// Setter for Row
+func (c *CPMK) SetRow(row int) {
+	c.Row = row
+}
+
+// Getter for Row
+func (c *CPMK) GetRow() int {
+	return c.Row
+}
+
+func New(name string, partisipasi float32, kuis float32, uts float32, uas float32, project float32) *CPMK {
 	return &CPMK{
+		Name: name,
 		Values: map[string]float32{
 			Partisipasi: partisipasi,
 			Kuis:        kuis,
@@ -26,8 +61,30 @@ func New(partisipasi float32, kuis float32, uts float32, uas float32, project fl
 			UAS:         uas,
 			Project:     project,
 		},
+		BeginCol: 1,
+		EndCol:   1,
+		Row:      2,
 	}
 }
+
+// func New(name string, partisipasi float32, kuis float32, uts float32, uas float32, project float32, row int, col int) *CPMK {
+// 	return &CPMK{
+// 		Name: name,
+// 		Values: map[string]float32{
+// 			Partisipasi: partisipasi,
+// 			Kuis:        kuis,
+// 			UTS:         uts,
+// 			UAS:         uas,
+// 			Project:     project,
+// 		},
+// 		if not beginCol{
+// 			BeginCol: 1,
+// 		} else {
+
+// 		BeginCol: row,
+// 		}
+// 	}
+// }
 
 func (c *CPMK) SetValue(column string, value float32) error {
 	if _, exists := c.Values[column]; !exists {
--- utils/writer/writer.go
+++ utils/writer/writer.go
@@ -4,71 +4,124 @@ import (
 	"fmt"
 
 	"github.com/hippocampa/obeobe/cpl"
-	model "github.com/hippocampa/obeobe/model"
+	"github.com/hippocampa/obeobe/model"
 	"github.com/xuri/excelize/v2"
 )
 
-func writeCPMK(f *excelize.File, m *model.Model, cpl cpl.CPL, styles map[StyleType]int) error {
+func writeCPMK(f *excelize.File, m *model.Model, cpl *cpl.CPL) error {
+	// TODO! CPMK BeginCol and EndCol
+	colCounter := cpl.BeginCol()
+	fmt.Println("Begin CPL Column is", colCounter)
 	for _, cpmk := range cpl.CPMK() {
-		cpKeyRow := 2
-		cpValueRow := 3
-
 		for k, v := range cpmk.Values {
 			if v != 0 {
-				colName, _ := excelize.ColumnNumberToName(m.LastCol())
-
-				// Write key cell
-				keyCell := fmt.Sprintf("%s%d", colName, cpKeyRow)
-				f.SetCellValue(m.GetSheetName(), keyCell, k)
-				ApplyStyle(f, styles, StyleBorder, m.GetSheetName(), keyCell)
-
-				// Write value cell
-				valueCell := fmt.Sprintf("%s%d", colName, cpValueRow)
+				cpl.SetEndCol(colCounter + 1)
+				colName, _ := excelize.ColumnNumberToName(cpl.EndCol())
+				keyLoc := fmt.Sprintf("%s%d", colName, 2)
+				f.SetCellValue(m.GetSheetName(), keyLoc, k)
+				valueCell := fmt.Sprintf("%s%d", colName, 3)
 				f.SetCellValue(m.GetSheetName(), valueCell, v/100.0)
-				ApplyStyle(f, styles, StylePercentage, m.GetSheetName(), valueCell)
+				colCounter++
 
-				m.SetLastCol(m.LastCol() + 1)
 			}
 		}
 	}
+	cpl.SetEndCol(colCounter - 1)
 	return nil
 }
 
 func writeCPL(f *excelize.File, m *model.Model) error {
-	styles, err := InitStyles(f)
-	if err != nil {
-		return err
-	}
-	for _, cpl := range m.CPL() {
-		colName, _ := excelize.ColumnNumberToName(m.LastCol())
-		row := 1
-		cell := fmt.Sprintf("%s%d", colName, row)
-		// Write CPL name
-		f.SetCellValue(m.GetSheetName(), cell, cpl.Name())
-		// Write CPMK
-		if err := writeCPMK(f, m, cpl, styles); err != nil {
-			return err
+	var lastEnd int
+	for i, cplItem := range m.CPL() {
+		if i == 0 {
+			cplItem.SetBeginCol(1)
+			cplItem.SetEndCol(0)
+		} else {
+			cplItem.SetBeginCol(lastEnd + 1)
+			cplItem.SetEndCol(lastEnd + 1)
 		}
 
-	}
+		colName, _ := excelize.ColumnNumberToName(cplItem.BeginCol())
+		cell := fmt.Sprintf("%s%d", colName, cplItem.Row())
+		f.SetCellValue(m.GetSheetName(), cell, cplItem.Name())
 
+		writeCPMK(f, m, cplItem)
+
+		lastEnd = cplItem.EndCol()
+	}
 	return nil
 }
 
 func WriteSheet(f *excelize.File, m *model.Model) error {
-	// Set sheet name
 	_, err := f.NewSheet(m.GetSheetName())
 	if err != nil {
 		return err
 	}
-	if err := writeCPL(f, m); err != nil {
-		return err
-	}
-	nextColForCPL, _ := excelize.ColumnNumberToName(m.LastCol() + 1)
-	fmt.Println("Next CPL Column is", nextColForCPL)
+	writeCPL(f, m)
 	return nil
-
 }
+
+// func writeCPMK(f *excelize.File, m *model.Model, cpl cpl.CPL, styles map[StyleType]int) error {
+// 	for _, cpmk := range cpl.CPMK() {
+// 		cpKeyRow := 2
+// 		cpValueRow := 3
+
+// 		for k, v := range cpmk.Values {
+// 			if v != 0 {
+// 				colName, _ := excelize.ColumnNumberToName(m.LastCol())
+
+// 				// Write key cell
+// 				keyCell := fmt.Sprintf("%s%d", colName, cpKeyRow)
+// 				f.SetCellValue(m.GetSheetName(), keyCell, k)
+// 				ApplyStyle(f, styles, StyleBorder, m.GetSheetName(), keyCell)
+
+// 				// Write value cell
+// 				valueCell := fmt.Sprintf("%s%d", colName, cpValueRow)
+// 				f.SetCellValue(m.GetSheetName(), valueCell, v/100.0)
+// 				ApplyStyle(f, styles, StylePercentage, m.GetSheetName(), valueCell)
+
+// 				m.SetLastCol(m.LastCol() + 1)
+// 			}
+// 		}
+// 	}
+// 	return nil
+// }
+
+// func writeCPL(f *excelize.File, m *model.Model) error {
+// 	styles, err := InitStyles(f)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	for _, cpl := range m.CPL() {
+// 		colName, _ := excelize.ColumnNumberToName(m.LastCol())
+// 		row := 1
+// 		cell := fmt.Sprintf("%s%d", colName, row)
+// 		// Write CPL name
+// 		f.SetCellValue(m.GetSheetName(), cell, cpl.Name())
+// 		// Write CPMK
+// 		if err := writeCPMK(f, m, cpl, styles); err != nil {
+// 			return err
+// 		}
+
+// 	}
+
+// 	return nil
+// }
+
+// func WriteSheet(f *excelize.File, m *model.Model) error {
+// 	// Set sheet name
+// 	_, err := f.NewSheet(m.GetSheetName())
+// 	if err != nil {
+// 		return err
+// 	}
+// 	if err := writeCPL(f, m); err != nil {
+// 		return err
+// 	}
+// 	nextColForCPL, _ := excelize.ColumnNumberToName(m.LastCol() + 1)
+// 	fmt.Println("Next CPL Column is", nextColForCPL)
+// 	return nil
+
+// }
 
 func SaveToExcel(f *excelize.File) error {
 	if err := f.SaveAs("test.xlsx"); err != nil {
